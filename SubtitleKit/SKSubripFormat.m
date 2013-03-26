69c71e41e3a642f8c31df4ca138aac2201dabe9f
xcvista <xcvista@me.com>
2013-03-27 02:18:19 +0800

    0.1.1-9r1: Writing SRT parser.
    Classical grammar and lex streategies used. This is a non-ad-hoc parser.

--- SubtitleKit/SKSubripFormat.m
+++ SubtitleKit/SKSubripFormat.m
@@ -8,7 +8,73 @@
 
 #import "SKSubripFormat.h"
 
+#define PtrAssign(ptr, value) \
+do {\
+typeof(ptr) _ptr = ptr;\
+if (_ptr)\
+*_ptr = value;\
+} while(0)
+
 @implementation SKSubripFormat
+
++ (BOOL)lexicalAnalyseNumber:(NSString *)line
+                     integer:(NSInteger *)number
+                      digits:(NSUInteger)digits
+              analysedDigits:(NSUInteger *)analysedDigits
+{
+    // Number = [+-][1-9][0-9]*
+    NSUInteger value = 0;
+    BOOL sign = NO; // NO = +, YES = -
+    NSUInteger state = 0;
+    NSUInteger i = 0;
+    
+    for (i = 0; i < MIN([line length], digits); i++)
+    {
+        unichar ch = [line characterAtIndex:i];
+        
+        switch (state)
+        {
+            case 0: // sign or digit
+            {
+                if (ch == '+')
+                {
+                    sign = NO;
+                    state = 1;
+                    break;
+                }
+                else if (ch == '-')
+                {
+                    sign = YES;
+                    state = 1;
+                    break;
+                }
+            }
+            case 1: // digit
+            {
+                if (ch >= '0' && ch <= 9)
+                {
+                    value *= 10;
+                    value += ch - '0';
+                }
+                else
+                {
+                    PtrAssign(number, value);
+                    PtrAssign(analysedDigits, i);
+                    return NO;
+                }
+            }
+            default:
+            {
+                PtrAssign(number, value);
+                PtrAssign(analysedDigits, i);
+                return NO;
+            }
+        }
+    }
+    PtrAssign(number, value);
+    PtrAssign(analysedDigits, i);
+    return YES;
+}
 
 + (BOOL)lexicalAnalyseTimetag:(NSString *)line
                     startTime:(NSTimeInterval *)start
@@ -23,14 +89,13 @@
 {
     NSMutableString *content = [fileContent mutableCopy]; // Char stream
     NSUInteger state = 0; // Refer to Subrip.md for info.
+    NSMutableDictionary *stateInfo = [NSMutableDictionary dictionary]; // Not gonna use those pesky ivars.
     
     while ([content length])
     {
         NSRange firstReturn = [content rangeOfCharacterFromSet:[NSCharacterSet newlineCharacterSet]];
         NSString *line = [[content substringToIndex:firstReturn.location] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
         [content deleteCharactersInRange:NSMakeRange(0, NSMaxRange(firstReturn))];
-        
-        NSMutableDictionary *stateInfo = [NSMutableDictionary dictionary]; // Not gonna use those pesky ivars.
         
         switch (state)
         {
